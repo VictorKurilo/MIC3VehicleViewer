1b3c3f7db21be8de6fed518947b8635ec566f36d
Victor Kurilo <33664583+VictorKurilo@users.noreply.github.com>
2024-06-05 22:25:41 +0200

    added material ui and theme (#1)
    Co-authored-by: victor kurilo <victor.kurilo@gmail.com>

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#63aaf1', 
+    },
+    secondary: {
+      main: '#F06543',
+    },
+  },
+});
+
+export default theme;
